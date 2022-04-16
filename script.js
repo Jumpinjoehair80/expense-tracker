fcbf475288a177408dbd3faf2491509dedffdd04
Joey Gibbons <GibbonsJoey95@gmail.com>
2022-04-15 21:44:18 -0500

    modified app.js

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const dateInput = document.getElementById('date-input')
 const amountInput = document.getElementById('amount-input')
 const btn = document.getElementById('btn')
 
+// Set localStorage
+function saveLocalStorage() {
+    localStorage.setItem('name', JSON.stringify(nameInput.value));
+    localStorage.setItem('date', JSON.stringify(dateInput.value));        localStorage.setItem('amount', JSON.stringify(amountInput.value));
+}
+console.log(saveLocalStorage())
+
+// Stored values of localStorage
+const storedName = localStorage.getItem('name')
+const storedDate = localStorage.getItem('date')
+const storedAmount = localStorage.getItem('amount')
+
+
+// Event listeners to add expenses to list
 btn.addEventListener('click', () => { 
     addExpense()
 })
@@ -14,13 +28,14 @@ amountInput.addEventListener('keydown', (e) => {
 })
 
 function addExpense() {
+    // Shows alert if all fields are not entered
     if(nameInput.value === '' || dateInput.value === '' || amountInput.value === '') {
         alert('Missing field!!')
         return false
     }
    
+    // Adds a row showing name, date, amount
     const tableData = document.getElementById('table-data')
-
     let row = tableData.insertRow(0)
     let cell1 = row.insertCell(0)
     let cell2 = row.insertCell(1)
@@ -29,7 +44,18 @@ function addExpense() {
     cell1.innerHTML = `${nameInput.value}`
     cell2.innerHTML = `${dateInput.value}`
     cell3.innerHTML = `$${amountInput.value}`
-    cell4.innerHTML = `<button class="btn-delete" id="btn-delete">X</button>`
+
+    // Local storage inserted into cells
+    if(saveLocalStorage != undefined) {
+        cell1.innerHTML = storedName
+        cell2.innerHTML = storedDate
+        cell3.innerHTML = storedAmount
+    }
+
+    // Adds delete btn to expense row
+    if(tableData) {
+        cell4.innerHTML = `<button class="btn-delete" id="btn-delete">X</button>`
+    }
         
     const deleteBtn = document.getElementById('btn-delete')
 
@@ -41,6 +67,7 @@ function addExpense() {
        })
       }
 
+    // Sets value of each input to clear after an expense is added  
     nameInput.value = ''
     dateInput.value = ''
     amountInput.value = ''
